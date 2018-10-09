1421b4eb9523b148dc7318d6df784bfd7f46b7d6
Raylene <smith.raylene@gmail.com>
2018-10-09 22:35:51 +0100

    Further refactoring

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,48 +4,41 @@ class GildedRose
     @items = items
   end
 
-  attr_reader :generic_items
+  attr_reader :items
 
   def update_all
-    update_aged_cheese
-    update_backstage_passes
-    update_generic_items
-  end
-
-  def update_aged_cheese
     @items.each do |item|
-      if item.name == "Aged Brie"
-        item.quality += 1 if item.sell_in > 0
-        item.quality += 2 if item.sell_in <= 0
-        item.quality = 50 if item.quality > 50
-        item.sell_in -= 1
-      end
+      update_aged_cheese(item) if item.name == "Aged Brie"
+      update_backstage_passes(item) if item.name.include?("Backstage passes")
+      update_generic_items(item) if item_is_generic?(item)
+      item.sell_in -= 1 if item.name != "Sulfuras"
     end
   end
 
-  def update_generic_items
-    @items.each do |item|
-      next if item.name == "Aged Brie"
-      next if item.name == "Sulfuras"
-      next if item.name.include?("Backstage passes")
-      item.quality -= 1 if item.sell_in > 0
-      item.quality -= 2 if item.sell_in <= 0
-      item.quality = 0 if item.quality < 0
-      item.sell_in -= 1
-    end
+  def update_aged_cheese(item)
+    item.quality += 1 if item.sell_in > 0
+    item.quality += 2 if item.sell_in <= 0
+    item.quality = 50 if item.quality > 50
   end
 
-  def update_backstage_passes
-    @items.each do |item|
-     if item.name.include?("Backstage passes")
-       item.quality += 1 if item.sell_in > 10
-       item.quality += 2 if item.sell_in.between?(6,10)
-       item.quality += 3 if item.sell_in.between?(1,5)
-       item.quality = 0 if item.sell_in <= 0
-       item.quality = 50 if item.quality > 50
-       item.sell_in -= 1
-     end
+  def update_generic_items(item)
+    item.quality -= 1 if item.sell_in > 0
+    item.quality -= 2 if item.sell_in <= 0
+    item.quality = 0 if item.quality < 0
+  end
+
+  def update_backstage_passes(item)
+     item.quality += 1 if item.sell_in > 10
+     item.quality += 2 if item.sell_in.between?(6,10)
+     item.quality += 3 if item.sell_in.between?(1,5)
+     item.quality = 0 if item.sell_in <= 0
+     item.quality = 50 if item.quality > 50
    end
- end
+
+   def item_is_generic?(item)
+     !(item.name == "Aged Brie" ||
+       item.name == "Sulfuras" ||
+       item.name.include?("Backstage passes"))
+   end
 
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,23 +3,59 @@ require 'item'
 
 describe GildedRose do
 
-  describe "#update_generic_items" do
-    it "does not change the name" do
+  describe "#item_is_generic?" do
+    it "returns true if item is generic" do
       item = Item.new("foo", 0, 0)
-      GildedRose.new([item]).update_generic_items
+      expect(GildedRose.new([item]).item_is_generic?(item)).to eq true
+    end
+
+    it "returns false if item is not generic" do
+      item = Item.new("Sulfuras", 1, 80)
+      expect(GildedRose.new([item]).item_is_generic?(item)).to eq false
+    end
+
+    it "returns false if item is not generic" do
+      item = Item.new("Backstage passes to test", 10, 10)
+      expect(GildedRose.new([item]).item_is_generic?(item)).to eq false
+    end
+
+  end
+
+  describe "update_all" do
+    it "does not change the name of an item" do
+      item = Item.new("foo", 0, 0)
+      GildedRose.new([item]).update_all
       expect(item.name).to eq "foo"
     end
 
     it "never lowers quality below 0" do
       item = Item.new("item", 0, 0)
-      GildedRose.new([item]).update_generic_items
+      GildedRose.new([item]).update_generic_items(item)
       expect(item.quality).to eq(0)
     end
 
+    context 'when item is a legendary item' do
+      it 'does not change sell_in' do
+        item = Item.new("Sulfuras", 1, 80)
+        GildedRose.new([item]).update_all
+        expect(item.sell_in).to eq(1)
+      end
+
+      it 'does not change quality' do
+        item = Item.new("Sulfuras", 1, 80)
+        GildedRose.new([item]).update_all
+        expect(item.quality).to eq(80)
+      end
+    end
+
+
+  end
+
+  describe "#update_generic_items" do
     context 'before sell_in' do
       it 'lowers quality by one after one day' do
         item = Item.new("item", 1, 1)
-        GildedRose.new([item]).update_generic_items
+        GildedRose.new([item]).update_generic_items(item)
         expect(item.quality).to eq(0)
       end
     end
@@ -27,22 +63,8 @@ describe GildedRose do
     context 'after sell_in' do
       it 'lowers quality by two after a day' do
         item = Item.new("item", 0, 2)
-        GildedRose.new([item]).update_generic_items
+        GildedRose.new([item]).update_generic_items(item)
         expect(item.quality).to eq(0)
-      end
-    end
-
-    context 'when item is not a generic item' do
-      it 'does not change sell_in' do
-        item = Item.new("Aged Brie", 1, 80)
-        GildedRose.new([item]).update_generic_items
-        expect(item.sell_in).to eq(1)
-      end
-
-      it 'does not change quality' do
-        item = Item.new("Sulfuras", 1, 80)
-        GildedRose.new([item]).update_generic_items
-        expect(item.quality).to eq(80)
       end
     end
   end
@@ -51,13 +73,13 @@ describe GildedRose do
     context 'before sell_in' do
       it 'raises quality by one after a day' do
         item = Item.new("Aged Brie", 1, 0)
-        GildedRose.new([item]).update_aged_cheese
+        GildedRose.new([item]).update_aged_cheese(item)
         expect(item.quality).to eq(1)
       end
 
       it 'never raises quality beyond 50' do
         item = Item.new("Aged Brie", 1, 50)
-        GildedRose.new([item]).update_aged_cheese
+        GildedRose.new([item]).update_aged_cheese(item)
         expect(item.quality).to eq(50)
       end
     end
@@ -65,31 +87,30 @@ describe GildedRose do
     context 'after sell_in' do
       it 'raises quality by two after a day' do
         item = Item.new("Aged Brie", 0, 0)
-        GildedRose.new([item]).update_aged_cheese
+        GildedRose.new([item]).update_aged_cheese(item)
         expect(item.quality).to eq(2)
       end
 
       it 'only raises quality to 50 when quality is at 49' do
         item = Item.new("Aged Brie", 0, 49)
-        GildedRose.new([item]).update_aged_cheese
+        GildedRose.new([item]).update_aged_cheese(item)
         expect(item.quality).to eq(50)
       end
     end
   end
 end
 
-
 describe '#update_backstage_passes' do
   context 'when sell_in is greater than 10' do
     it 'increases quality by one after each day' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 0)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(1)
     end
 
     it 'never increases quality beyond 50' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(50)
     end
   end
@@ -97,13 +118,13 @@ describe '#update_backstage_passes' do
   context 'when sell_in is less than or equal to 10 but more than 5' do
     it 'increases quality by two after each day' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(2)
     end
 
     it 'only raises quality to 50 when quality is at 49' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 49)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(50)
     end
   end
@@ -111,13 +132,13 @@ describe '#update_backstage_passes' do
   context 'when sell_in is less than or equal to 5 but more than 0' do
     it 'increases quality by three after each day' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(3)
     end
 
     it 'only raises quality to 50 when quality is at 48 or more' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 48)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(50)
     end
   end
@@ -125,13 +146,13 @@ describe '#update_backstage_passes' do
   context 'after sell_in' do
     it 'sets quality to 0' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(0)
     end
 
     it 'does not change quality' do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)
-      GildedRose.new([item]).update_backstage_passes
+      GildedRose.new([item]).update_backstage_passes(item)
       expect(item.quality).to eq(0)
     end
   end
